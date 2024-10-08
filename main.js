af4ebdd108609b7562bd004624566e534c0b666e
salvoparisi <psalvo065@gmail.com>
2024-10-08 14:00:44 +0200

    add bonus

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let i=0;
 function changeimg(){
     let img = document.getElementById('lampadina')
+    let button = document.getElementById('bottone')
     if (i%2==0) {
         img.src = './img/yellow_lamp.png'
+        button.textContent = 'SPEGNI'
     }else{
         img.src = './img/white_lamp.png'
+        button.textContent = 'ACCENDI'
     }
     console.log("lampadina cambiata");
     i++;
